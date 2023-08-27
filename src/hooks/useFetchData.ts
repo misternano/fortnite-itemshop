c00868c3094fc2b4e0a37f0677edd21f3f7572e8
Nano <cjstitt@icloud.com>
2023-08-26 21:17:08 -0400

    Update useFetchData.ts

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -2,21 +2,21 @@
 import { useEffect, useState } from "react";
 import type { ShopData } from "@types";
 
-const useFetchData = (type: string) => {
+const useFetchData = (path: string) => {
 	const [data, setData] = useState<ShopData | null>(null);
 	const [error, setError] = useState<string | null>(null);
 
 	useEffect(() => {
 		(async () => {
 			try {
-				const response = await fetch(`https://fn.nanos.club/api/${type}`);
+				const response = await fetch(`https://fn.nanos.club/api/${path}`);
 				const body = await response.json();
 				setData(body.data);
 			} catch (err) {
 				setError((err as { message: string }).message);
 			}
 		})();
-	});
+	}, []);
 
 	return { data, error };
 };
