21da6200831e2f855df2a44f3077929ea9e130e8
misternano <cjstitt@icloud.com>
2023-12-01 21:03:16 -0500

    empty shop screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,30 +7,41 @@ const Home = () => {
 
 	return (
 		<>
-			<div className="w-[90%] mx-auto my-6 flex flex-col gap-12">
-				{data?.daily && data?.daily?.length > 0 &&
+			{data?.featured?.length !== 0 ? (
+				<div className="w-[90%] mx-auto my-6 flex flex-col gap-12">
+					{data?.daily && data?.daily?.length > 0 &&
+						<section className="flex flex-col gap-4 justify-center items-center">
+							<h2 className="w-full p-1 text-center bg-neutral-900 rounded drop-shadow">
+								Daily
+							</h2>
+							<ShopLayout tab="daily" />
+						</section>
+					}
+					<section className="flex flex-col gap-4 justify-center items-center">
+						{data?.daily && data?.daily?.length > 0 &&
+							<h2 className="w-full p-1 text-center bg-neutral-900 rounded drop-shadow">
+								Featured
+							</h2>
+						}
+						<ShopLayout tab="featured" />
+					</section>
 					<section className="flex flex-col gap-4 justify-center items-center">
 						<h2 className="w-full p-1 text-center bg-neutral-900 rounded drop-shadow">
-							Daily
+							Bundles
 						</h2>
-						<ShopLayout tab="daily" />
+						<ShopLayout tab="bundles" />
 					</section>
-				}
-				<section className="flex flex-col gap-4 justify-center items-center">
-					{data?.daily && data?.daily?.length > 0 &&
-						<h2 className="w-full p-1 text-center bg-neutral-900 rounded drop-shadow">
-							Featured
-						</h2>
-					}
-					<ShopLayout tab="featured" />
-				</section>
-				<section className="flex flex-col gap-4 justify-center items-center">
-					<h2 className="w-full p-1 text-center bg-neutral-900 rounded drop-shadow">
-						Bundles
-					</h2>
-					<ShopLayout tab="bundles" />
-				</section>
-			</div>
+				</div>
+			) : (
+				<div className="mt-[35vh] mb-[55vh] grid place-items-center">
+					<h1 className="font-burbank text-xl">No, really. The shop is empty.</h1>
+					<div className="flex flex-row gap-2">
+						<a href="https://status.epicgames.com/" target="norel noopen" className="text-lg text-purple-400 hover:text-purple-500 underline transition-colors">Fortnite Status</a>
+						<div className="h-1/2 w-0.5 my-auto bg-neutral-500 rounded-full text-xs">&nbsp;</div>
+						<a href="https://x.com/fortnitestatus/" target="norel noopen" className="text-lg text-purple-400 hover:text-purple-500 underline transition-colors">Twitter</a>
+					</div>
+				</div>
+			)}
 		</>
 	);
 };
