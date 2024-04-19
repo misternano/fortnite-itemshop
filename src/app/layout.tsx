e8842c90259f074c7e3a1db15432b4c9786d1ff6
misternano <cjstitt@icloud.com>
2024-04-18 21:29:25 -0400

    gonna cry

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Navigation, Footer } from "@components";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
 	title: "Fortnite Item Shop",
