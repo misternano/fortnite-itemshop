3588fd14c06e9ec146480b3f2232f7b37e121746
misternano <cjstitt@icloud.com>
2024-04-26 20:19:55 -0400

    changed vbuck svg to its own component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,13 @@ import Footer from "./Footer";
 import ShopLayout from "./ShopLayout";
 import UpcomingLayout from "./UpcomingLayout";
 import ItemCard from "./ItemCard";
+import VBuckIcon from "./VBuckIcon";
 
 export {
 	Navigation,
 	Footer,
 	ShopLayout,
 	UpcomingLayout,
-	ItemCard
+	ItemCard,
+	VBuckIcon
 };
