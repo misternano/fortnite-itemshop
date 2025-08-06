aaf31bee6a64e5d560bbfdec36a186a3b0912ba4
Nano <cjstitt@icloud.com>
2025-08-06 19:00:40 -0400

    refactor: updating api endpoints

--- src/hooks/useFetchData.ts
+++ src/hooks/useFetchData.ts
@@ -9,7 +9,7 @@ const useFetchData = (path: string) => {
 	useEffect(() => {
 		(async () => {
 			try {
-				const response = await fetch(`https://fn.nanos.club/api/${path}`);
+				const response = await fetch(`https://api.nanos.club/fn/${path}`);
 				const body = await response.json();
 				setData(body.data);
 			} catch (err) {
